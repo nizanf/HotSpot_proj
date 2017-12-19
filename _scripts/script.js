2e8f4ed3cb0200b6fdfa89a6f6ebe7b0e6a5c08d
nizanf <33806916+nizanf@users.noreply.github.com>
2017-12-19 15:34:58 +0200

    update

---
+++ _scripts/script.js
@@ -0,0 +1,23 @@
+
+function MyProfile(){
+    var name=document.getElementsByName("user").value;
+    var password=document.getElementsByName("password").value;
+
+}
+
+
+
+// function myMap() {
+// for (i=0 ;i<100; i++ ){
+//     Document.write("hello");
+// }
+//      var mapProp= {
+//         center:new google.maps.LatLng(32.121678, 34.791143),
+//         zoom:10,
+//         zoomControl:true
+//     };
+//     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+// <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIn9K5Niimp_WDiNC8ckPUcKdi_RxYLHM&callback=myMap"></script>
+
+// myMap();
